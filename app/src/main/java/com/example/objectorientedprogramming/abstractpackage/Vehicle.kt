c06143de96d3fbf75fd23b36400200f11da427c1
Aleksandra Brzoza <alebrz00104@gmail.com>
2025-02-26 11:07:48 +0100

    add abstract package, classes and file

---
+++ app/src/main/java/com/example/objectorientedprogramming/abstractpackage/Vehicle.kt
@@ -0,0 +1,24 @@
+package com.example.objectorientedprogramming.abstractpackage
+
+abstract class Vehicle {
+
+    // abstract function
+    abstract fun vehicleName(name:String) : String
+
+    // non-abstract function
+    fun vehicleType(type:String) : String {
+        return type
+    }
+
+
+    // abstract property
+    abstract var model:Int
+
+    // non-abstract
+    var speed:Int? = null
+
+
+
+
+
+}
